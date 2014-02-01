fdc67dfcbe901a710f3b2225fb34ee35c8ef4915
Nancy <nancytechy@gmail.com>
2014-02-01 20:18:39 +0000

    working out how the hell to test ranges

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,3 +1,35 @@
 window.hashOut = function() {
   this.selections = 0;
 };
+
+
+(function($){
+
+  getMousedSelection = function() {
+      highlight = "";
+
+      if (window.getSelection) {
+          var choice = window.getSelection();
+          if (choice.rangeCount) {
+              var container = document.createElement("div");
+              for (var i = 0, len = choice.rangeCount; i < len; ++i) {
+                  container.appendChild(choice.getRangeAt(i).cloneContents());
+              }
+              highlight = container.innerHTML;
+          }
+
+      } else if (document.selection) {
+          if (document.selection.type == "Text") {
+              highlight = document.selection.createRange().htmlText;
+          }
+      }
+      console.log(highlight);
+      return highlight;
+      };
+
+    $('#article').mouseup(function(e) {
+        getMousedSelection();
+    });
+
+})($);
+
